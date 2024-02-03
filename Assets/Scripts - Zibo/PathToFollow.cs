b127ca14692f1e66b7038ee1ba242cd20eb383a1
sundzibo <d.z.sun@lancaster.ac.uk>
2024-02-03 02:27:08 +0000

    Implementation of a NavMesh path with checkpoints.

---
+++ Assets/Scripts - Zibo/PathToFollow.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class PathToFollow : MonoBehaviour
+{
+    public Transform path;          // A gameobject with a number of positions on a path as children
+    public NavMeshAgent agent;
+    public bool startOnAwake;
+
+    List<Vector3> points = new List<Vector3>();
+    int nextPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in path) {
+            points.Add(child.position);
+        }
+
+        nextPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!startOnAwake) return;
+
+        if (nextPoint < points.Count) {
+            agent.SetDestination(points[nextPoint]);
+        }
+
+        // reached point
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= 2)
+            {
+                nextPoint++;
+            }
+        }
+    }
+
+    public void StartPath() {
+        startOnAwake = true;
+    }
+}
