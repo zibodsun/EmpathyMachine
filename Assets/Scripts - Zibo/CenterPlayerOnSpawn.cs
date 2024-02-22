ab998898acb399a729871489d2fa974c66d6d780
sundzibo <d.z.sun@lancaster.ac.uk>
2024-02-22 01:53:03 +0000

    Improved XROrigin out-of-bounds correction.

--- Assets/Scripts - Zibo/CenterPlayerOnSpawn.cs
+++ Assets/Scripts - Zibo/CenterPlayerOnSpawn.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 /*
  *  Stops XROrigin from spawning out of bounds
@@ -9,18 +10,38 @@ using UnityEngine;
 public class CenterPlayerOnSpawn : MonoBehaviour
 {
     public Transform spawn;
+    public LocomotionSystem locomotionSystem;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(Center());
+        locomotionSystem.gameObject.SetActive(false);
+        StartCoroutine(Center(0.1f));
+    }
+    
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, spawn.position) > 100) {
+            transform.position = spawn.position;
+            Center();
+        }
     }
 
-    IEnumerator Center() {
-        yield return new WaitForSeconds(1f);
+    IEnumerator Center(float wait) {
+        yield return new WaitForSeconds(wait);
         XROrigin xrorigin = GetComponent<XROrigin>();
 
         xrorigin.MoveCameraToWorldLocation(new Vector3(spawn.position.x, xrorigin.Camera.transform.position.y, spawn.position.z));
         xrorigin.MatchOriginUpCameraForward(spawn.up, spawn.forward);
+        locomotionSystem.gameObject.SetActive(true);
+    }
+
+    void Center()
+    {
+        XROrigin xrorigin = GetComponent<XROrigin>();
+
+        xrorigin.MoveCameraToWorldLocation(new Vector3(spawn.position.x, xrorigin.Camera.transform.position.y, spawn.position.z));
+        xrorigin.MatchOriginUpCameraForward(spawn.up, spawn.forward);
+        locomotionSystem.gameObject.SetActive(true);
     }
 }
